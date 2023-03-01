3beb8ea1f3290e2500bb88e35f23858efad2878a
sahil.garg78272026@gmail.com <sahil.garg78272026@gmail.com>
2023-03-01 11:44:07 +0530

    port_scanner_frst

---
+++ port_scanner.py
@@ -0,0 +1,29 @@
+import socket
+
+def scan_ports(host, ports):
+    # Create a socket object for each port to check
+    results = []
+    for port in ports:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(1)
+        try:
+            s.connect((host, port))
+            results.append(f"Port {port} is open")
+        except:
+            pass
+        finally:
+            s.close()
+            
+    return results
+
+# Example usage
+host = "127.0.0.1"
+ports = [21, 22, 23, 8000, 443]
+print("Scanning open ports on", host)
+results = scan_ports(host, ports)
+if len(results) > 0:
+    print("Found", len(results), "open ports:")
+    for result in results:
+        print(result)
+else:
+    print("No open ports found.")
